3a5b5b1f7e60ea238a9926d7fa7b5dea7470d4f4
Vinh Nguyen <vinh.nguyen@codestringers.com>
2021-10-12 22:32:26 +0700

    Add full Defined Doc UI

--- src/containers/DefineDocProcess/DefineDoc.js
+++ src/containers/DefineDocProcess/DefineDoc.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './DefineDoc.css';
 import group from './group178.png';
+import trash from './trash-2.png';
+import Button from '@mui/material/Button';
 
 function DocumentItem(props) {
     return (
         <div className="doc-item">
-            <div>
-                <img src={group} className="group" alt="group" />
-                <div className="doc-name-item" >{props.name}</div>
-                <div>Require Signature</div>
-                <div></div>
+            <div className="document doc-item-1">
+                <div className="doc-name-item" ><img src={group} className="group" alt="group" /> {props.name}</div>
+                <div className="doc-name-item-require">Require Signature</div>
+                <div style={{ margin: '12px 0px 0px 72px' }}>
+                    <label className="switch">
+                        <input type="checkbox"></input>
+                        <span className="slider"></span>
+                    </label>
+                </div>
+            </div>
+            <div className="document doc-item-2">
+                <div className="doc-item-2-label">
+                    <div className="doc-item-2-btn">ADD PROCESS <span style={{ marginLeft: '37px' }}><img src={trash} className="trash" alt="trash" width="24px" height="24px" /></span></div>
+                </div>
+
             </div>
         </div>
     )
 }
+
 function DefineDoc(props) {
     return (
         <div style={{ margin: '40px 0px 0px 120px' }}>
@@ -23,6 +36,10 @@ function DefineDoc(props) {
             </div>
             <DocumentItem name="Document A.pdf" />
             <DocumentItem name="Document B.pdf" />
+            <div style={{ margin: '20px 270px 0px', float: 'right' }}>
+                <Button variant="outlined" className="btn-back">Back</Button>
+                <Button variant="outlined" className="btn-continue" style={{ marginLeft: '16px' }}>Continue</Button>
+            </div>
         </div>
     );
 }
